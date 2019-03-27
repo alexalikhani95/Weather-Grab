f6cb74da192f4bcb955cbacd1b8b11e2ed5fa799
Alex <alikhanialex@gmail.com>
2019-03-27 22:36:22 +0000

    basic webpage functionality

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Titles extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Weather Grab</h1>
+                <p>Discover weather, conditions and more</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
